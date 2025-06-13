821372d9e1f2b192b1113a6c4c207f0bed5b7afa
AbdoulIdriss <abdoulben44@gmail.com>
2025-06-13 10:23:16 +0100

    created service logic

--- src/app/services/person.service.ts
+++ src/app/services/person.service.ts
@@ -1,9 +1,31 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Person } from '../models/person';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PersonService {
 
-  constructor() { }
+  constructor( private http: HttpClient ) { }
+
+  private api = 'http://localhost:3000/persons';
+  
+  getAllPerson():Observable<Person[]> {
+    return this.http.get<Person[]>(this.api); //function to get all persons
+  }
+
+  createPerson( p : Person):Observable<Person> {
+    return this.http.post<Person>(this.api , p); //function to create a person
+  }
+
+  updatePerson( p: Person ):Observable<Person> {
+    return this.http.put<Person>(`${this.api}/${p.id}` , p); //function to modify person info 
+  }
+
+  deletePerson( id :number ):Observable<void> {
+    return this.http.delete<void>(`${this.api}/${id}`); //function to delete person
+  }
+
 }
--- src/app/services/todo.service.ts
+++ src/app/services/todo.service.ts
@@ -1,9 +1,30 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Todo } from '../models/todo';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TodoService {
 
-  constructor() { }
+  constructor( private http: HttpClient ) { }
+
+  private api = 'http://localhost:3000/todos';
+  
+  getAllPerson():Observable<Todo[]> {
+    return this.http.get<Todo[]>(this.api); //function to get all Todos
+  }
+
+  createTodo( t : Todo):Observable<Todo> {
+    return this.http.post<Todo>(this.api , t); //function to create a Todo
+  }
+
+  updateTodo( t: Todo ):Observable<Todo> {
+    return this.http.put<Todo>(`${this.api}/${t.id}` , t); //function to modify Todo info 
+  }
+
+  deleteTodo( id :number ):Observable<void> {
+    return this.http.delete<void>(`${this.api}/${id}`); //function to delete Todo
+  }
 }

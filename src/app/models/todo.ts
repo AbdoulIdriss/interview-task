9f854412af913bdd682db2013348f1f083de9188
AbdoulIdriss <abdoulben44@gmail.com>
2025-06-13 10:01:42 +0100

    added models, services  and json-server

---
+++ src/app/models/todo.ts
@@ -0,0 +1,15 @@
+import { Person } from "./person";
+
+export enum Priority { Facile = "Facile", Moyen = "Moyen", Difficile = "Difficile" }
+export enum Label { HTML = "HTML", CSS = "CSS", NodeJS = "NODE JS", JQUERY = "JQUERY" }
+
+export interface Todo {
+    id: number;
+    title: string;
+    person: Person;
+    startDate: string;
+    endDate?: string;
+    priority: Priority;
+    labels: Label[];
+    description: string;
+}
